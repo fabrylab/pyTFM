7d6d4ebd80e8331f09dd2d7bda22ce3403bea8da
Andreas Bauer <Andreas.Bauer.de@gmx.net>
2019-09-17 09:19:32 +0200

    fixed database name

--- build_cdb_database_TFM.py
+++ build_cdb_database_TFM.py
@@ -3,4 +3,4 @@
 from andreas_TFM_package.TFM_functions_for_clickpoints import *
 #folder="/media/user/GINA1-BK/data_traktion_force_microscopy/TFM_cell_patches/WT2shift/out_clickpoints"
 folder=os.getcwd()
-frames=setup_database_for_tfm(folder,"database3.cdb",return_db=False)
+frames=setup_database_for_tfm(folder,"database.cdb",return_db=False)
