3667544f933a7468b84f5df0fc379e046b694aef
Andreas Bauer <Andreas.Bauer.de@gmx.net>
2019-10-14 11:44:54 +0200

    corrected import

--- analysis_and_testing/build_cdb_database_TFM.py
+++ analysis_and_testing/build_cdb_database_TFM.py
@@ -1,4 +1,4 @@
-from andreas_TFM_package.TFM_functions_for_clickpoints import *  # must be on top becauseof some matplotlib backend issues
+from andreas_TFM_package.database_functions import *  # must be on top becauseof some matplotlib backend issues
 import os
 folder=os.getcwd()
 #folder="/media/user/GINA1-BK/data_traktion_force_microscopy/TFM_cell_patches/WT2shift/out_clickpoints"
