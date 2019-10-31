af42885bdc6082a21a713546a38b31b8617d40b5
AndreasBauerGit <54469614+AndreasBauerGit@users.noreply.github.com>
2019-10-31 15:22:24 +0100

    removed return values in setup_database_for_tfm with default parameters

--- analysis_and_testing/build_cdb_database_TFM.py
+++ analysis_and_testing/build_cdb_database_TFM.py
@@ -2,4 +2,4 @@ from andreas_TFM_package.database_functions import *  # must be on top becauseof
 import os
 folder=os.getcwd()
 #folder="/media/user/GINA1-BK/data_traktion_force_microscopy/TFM_cell_patches/WT2shift/out_clickpoints"
-frames=setup_database_for_tfm(folder,"database.cdb",return_db=False)
+setup_database_for_tfm(folder,"database.cdb",return_db=False)
