951d1104fc3571fb7ed441e37a40d2dbd5be0651
AndreasBauerGit <54469614+AndreasBauerGit@users.noreply.github.com>
2019-10-31 15:21:26 +0100

    removed return values in setup_database_for_tfm with defualt parameters

--- build_cdb_database_TFM.py
+++ build_cdb_database_TFM.py
@@ -3,7 +3,7 @@
 from andreas_TFM_package.database_functions import *
 #folder="/media/user/GINA1-BK/data_traktion_force_microscopy/TFM_cell_patches/WT2shift/out_clickpoints"
 folder=os.getcwd()
-frames=setup_database_for_tfm(folder,"database.cdb",return_db=False)
+setup_database_for_tfm(folder,"database.cdb",return_db=False)
 
 #setup_database_for_tfm(r'C:\Users\User\Desktop\test_andi', r'data.cdb', key1="Series088_t17_z051_ch00.*",
 #                           key2="Series089_t12_z051_ch00.*", key3="Series088_t17_z051_ch01.*",frame_key="frame(\d)")
