5cdc968b6e2b1f795ebb468d98c80d335fdf0720
Andreas Bauer <Andreas.Bauer.de@gmx.net>
2020-03-06 09:20:51 +0100

    updated requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author='Andreas Bauer',
     author_email='andreas.b.bauer@fau.de',
     license='',
-    install_requires=['numpy' ,'cython','openpiv==0.20.8','scipy', 'scikit-image', 'matplotlib', 'tqdm', 'solidspy','clickpoints >= 1.9.0'], #[clickpoints 18.3] could be rather problematic
+    install_requires=['numpy' ,'cython','openpiv==0.20.8','scipy', 'scikit-image', 'matplotlib >= 2.1.2', 'tqdm', 'solidspy','clickpoints >= 1.9.0'], #[clickpoints 18.3] could be rather problematic
     keywords = ['traction force microscopy','finite elements'],
     classifiers = [],
     include_package_data=True,
