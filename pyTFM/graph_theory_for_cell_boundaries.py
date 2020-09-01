fixed rare stack overflow error when finding borders
replaced recursive function that explores a graph between two intersection points with while logic

pyTFM/graph_theory_for_cell_boundaries.py
@@ -151,7 +151,9 @@ def find_lines_simple(graph):
 
 
 def find_path(graph, start, end, path=[]):
+
     '''
+    recursive function
     finds a path (not necessarily the shortest one) through a graph from start to an end node (not necessarily the closest one).
 
     :param graph: dict, graph
@@ -174,8 +176,10 @@ def find_path(graph, start, end, path=[]):
 
 
 def find_path_to_endpoint(graph, start, path=[], first=False):
+
     '''
-    finds a path to a (not specific) point with onely one neighbour
+    recursive function
+    finds a path to a (not specific) point with only one neighbour
 
     :param graph: dict, graph
     :param start: int, start point, must be a key in the graph
@@ -196,14 +200,16 @@ def find_path_to_endpoint(graph, start, path=[], first=False):
     return path  # only partial path
 
 
-def find_line_segement(graph, start, path=[], left_right=0):
+def find_line_segement_recursive(graph, start, path=[], left_right=0):
     '''
-    recursive function, finds path from a point going from the first or second neigbour until
+    ---> would sometimes cause stack overflow/recursion error
+    recursive function
+    finds path from a point going from the first or second neigbour until
     it reaches an intersection (point with three neighbours)
     :param graph: graph as a dictionary
     :param start: start point
     :param path: path as list of nodes
-    :param left_right: define wich neighbour from start to explore (0 or 1
+    :param left_right: define which neighbour from start to explore (0 or 1
     :return:
     '''
     if len(graph[start]) > 2:  # stop if intersection (point with 3 neighbours is reached
@@ -217,10 +223,51 @@ def find_line_segement(graph, start, path=[], left_right=0):
     else:
         new_p = new_ps[new_ps != path[-2]][0]  # next point that wasn't the previous point
     # recursive function
-    newpath = find_line_segement(graph, new_p, path)  # next step
+    newpath = find_line_segement_recursive(graph, new_p, path)  # next step
 
     if newpath:
         return newpath  # return if recursion is completed
+
+
+def find_line_segement(graph, start, path=None, left_right=0):
+    '''
+    ---> would sometimes cause stack overflow/recursion error
+    recursive function
+    finds path from a point going from the first or second neigbour until
+    it reaches an intersection (point with three neighbours)
+    :param graph: graph as a dictionary
+    :param start: start point
+    :param path: path as list of nodes
+    :param left_right: define which neighbour from start to explore (0 or 1
+    :return:
+    '''
+
+    # first point
+    path = []
+    path.append(start)
+    new_ps = graph[start]
+    new_p = new_ps[left_right]  # just choose one point to the left or right
+    # break of if we already hit another intersection
+    if len(graph[new_p]) > 2:
+        return path
+    else:
+        path.append(new_p)
+
+    while True:  # stop if intersection (point with 3 neighbours) is reached
+        new_ps = graph[new_p]
+        new_p = [p for p in new_ps if p not in path]
+        new_p = new_p[0]
+        # check if the point has more (or less) then 2 neighbours.
+        # should be more then 2 to indicate intersection
+        if len(graph[new_p]) != 2:
+            break
+        else:
+            path.append(new_p)
+    return path
+
+
+
+
 
 
 def mask_to_graph(mask, d=np.sqrt(2)):
@@ -241,9 +288,9 @@ def mask_to_graph(mask, d=np.sqrt(2)):
     return graph, points
 
 
-def identify_line_segments(graph):  ## could be  abit improved , sometimes a few points at the edges are left out...
+def identify_line_segments(graph, points):  #
     '''
-        function to identify all line segents(representing individual cell boundaries. Segments are returnt as a dictionary
+        function to identify all line segments (representing individual cell boundaries. Segments are returned as a dictionary
         with an id as key and a list of points (referring to the points array) that are included in the line. The
         points are in correct order already
     :param graph:
@@ -253,20 +300,22 @@ def identify_line_segments(graph):  ## could be  abit improved , sometimes a few
 
     lines_dict = {}
     n = 0  # counter in while loop
-    all_points = list(graph.keys())  # all points that are (remaining in the graph)
-    intersect_ps = [key for key, values in graph.items() if len(values) > 2]  # fining intersection points
+    all_points = list(graph.keys())  # all points in the graph
+    intersect_ps = [key for key, values in graph.items() if len(values) > 2]  # finding intersection points
     if len(intersect_ps) == 0:
-        raise FindingBorderError("Can't I dentify cell borders.")
+        raise FindingBorderError("Can't identify internal cell borders.")
     remaining = set(all_points) - set(intersect_ps)  # remaining point ids
     while len(remaining) > 0:  # stop when all points are assigned to intersect or line segment
         start = next(iter(remaining))  # first point of remaining points
+        # finding a line segment
         line_seg = list(reversed(find_line_segement(graph, start=start, left_right=0)))[:-1] + find_line_segement(graph,
                                                                                                                   start=start,
-                                                                                                                  left_right=1)  # find line segment
-        remaining -= set(line_seg)  # updating remaining  list
-        ## maybe reomve this codition??
-        if len(
-                line_seg) > 1:  # avoid single point lines, which are not usefull (cant really get normal vectors from them....)
+                                                                                                                  left_right=1)
+
+        remaining -= set(line_seg)  # updating remaining list
+
+        # avoid single point lines, which are not usefull (cant really get normal vectors from them)
+        if len(line_seg) > 1:
             lines_dict[n] = line_seg
             n = n + 1
 
@@ -356,7 +405,7 @@ def find_neighbor_lines(graph, start_ps, other_endpoint, own_points, end_points,
     next_ps = [p for ps in next_ps for p in ps]  # flatten
     next_ps = list(np.unique(next_ps))  # removing duplication
 
-    # avoid conecting the two endpoints on own line directly
+    # avoid connecting the two endpoints on own line directly
     # only true in first "iteration layer"
     if other_endpoint in next_ps and len(visited) == 1:
         next_ps.remove(other_endpoint)

pyTFM/grid_setup_solids_py.py
@@ -144,7 +144,7 @@ def arrange_lines_from_endpoints(cells_lines, lines_endpoints_com):
         while not np.isnan(eps).all():
             point = copy.deepcopy(eps[p_ind1, p_ind2])  # extracting an endpoint
             eps[p_ind1, p_ind2] = np.nan  # remove the point from array
-            # find second occurrence, by taking the norm of the diffrece between the current point and all other points
+            # find second occurrence, by taking the norm of the diffrence between the current point and all other points
             # this should be zero
             np_ind1, np_ind2 = np.array(np.where(np.linalg.norm(eps - point, axis=2) == 0)).T[0]
             new_line_ids.append(line_ids[np_ind1])  # note corresponding line_ids
@@ -184,7 +184,7 @@ def center_of_mass_cells(cells_points, points):
     return cells_com
 
 
-def remove_circular_line(lines_endpoints_com, lines_points, lines_endpoints):
+def remove_circular_line(allLines_points, lines_endpoints_com, lines_points, lines_endpoints):
     '''
     finds lines that are circular by checking if the first and second endpoint are identical. The lines are
     delted from all input dictionaries
@@ -202,6 +202,7 @@ def remove_circular_line(lines_endpoints_com, lines_points, lines_endpoints):
         del lines_endpoints_com[l_id]
         del lines_points[l_id]
         del lines_endpoints[l_id]
+        del allLines_points[l_id]
 
 
 def interpolate_cell_area(cells_area, shape):
@@ -226,7 +227,7 @@ class Cells_and_Lines:
         # masks, graph and points including dead-end lines // this distinction is mostly due to historic reasons
         self.mask_boundaries_wp = mask_boundaries
         self.inter_shape = shape
-        # graph as a dictionray with key=ponit id, values: ids of neighbouring points
+        # graph as a dictionary with key = point id, values: ids of neighbouring points
         # any point id is the index in the points array (contains coordinate of these points
         self.graph_wp = graph
         self.points_wp = points
@@ -241,8 +242,8 @@ class Cells_and_Lines:
         # points as dictionary with key=points id, values: points coordinates
         self.points_dict = {i: self.points[i] for i in range(len(self.points))}
 
-        # lines as a dictionary with key=line id, values: ids of containg points (in correct order)
-        self.lines_points = identify_line_segments(self.graph)
+        # lines as a dictionary with key=line id, values: ids of containing points (in correct order)
+        self.lines_points = identify_line_segments(self.graph, self.points_interpol)
 
         # cells as a dictionary with key=cell id, values: ids of containing points (not ordered)
         self.cells_points, self.cells_area = identify_cells(self.mask_boundaries,
@@ -263,24 +264,26 @@ class Cells_and_Lines:
         self.lines_endpoints_com, self.lines_endpoints = find_exact_line_endpoints(self.lines_points, self.points,
                                                                                    self.graph)
 
-        # removing all lines that are predicted to be circular. Mostly a problem for very short lines
-        remove_circular_line(self.lines_endpoints_com, self.lines_points, self.lines_endpoints)
+        #self.simple_line_plotting(self.allLines_points, subset=np.inf)
+        #for p in self.lines_endpoints_com.values():
+        #    plt.plot(p[0][1],p[0][0],"o")
+        #    plt.plot(p[1][1], p[1][0], "o")
 
-        # dictionary with both dead end lines and circular lines
+        # removing all lines that are predicted to be circular. Mostly a problem for very short lines
+        remove_circular_line(self.allLines_points, self.lines_endpoints_com, self.lines_points, self.lines_endpoints)
 
         # center of mass of cells, calculated only from the hull points
         self.cells_com = center_of_mass_cells(self.cells_points, self.points)
         # dictionary to associate cells with correct lines, key is cell id, value is line_id
-        self.cells_lines = defaultdict(list)  ## improve by using endpoints for cell_line_association
+        self.cells_lines = defaultdict(list)
         # dictionary to associate cells with correct lines, key is line id, value is cell
 
         self.lines_cells = defaultdict(list)
         for l_id, l in self.lines_points.items():
             for c_id, c in self.cells_points.items():
-                if l[0] in c:
+                if l[int(len(l)/2)] in c:
                     self.cells_lines[c_id].append(l_id)
                     self.lines_cells[l_id].append(c_id)
-
         # using the new endpoints to arrange lines in the correct way
         self.cells_lines = arrange_lines_from_endpoints(self.cells_lines, self.lines_endpoints_com)
 
@@ -405,9 +408,9 @@ class Cells_and_Lines:
             ps = np.array(ps)
             color = colors[np.array([l in line_ids for line_ids in line_classifier])][0]
             p_indices = np.array(list(range(len(ps))))  # all indicces
-            # randomly choising a few indices, without first and laast index
+            # randomly choosing a few indices, without first and laast index
             ps_select = np.random.choice(p_indices[1:-1], size=int((len(ps) - 2) * sample_factor), replace=False)
-            ps_select = np.append(ps_select, p_indices[np.array([1, -1])])  # adding back first and last index
+            ps_select = np.append(ps_select, p_indices[np.array([0, -1])])  # adding back first and last index
 
             plt.plot(self.points[ps][:, 1], self.points[ps][:, 0], "o", color=color)
             for p in ps[ps_select]:  # labeling selected points
@@ -527,7 +530,7 @@ def find_borders(mask, shape, raise_error=True, type="colony", min_length=0):
     mask_boundaries = skeletonize(mask.astype(int))
     # converting mask to graph object
     graph, points = mask_to_graph(mask_boundaries)
-    # finding dead ends: cell borders wich dont connect to other cell borders at one end:
+    # finding dead ends: cell borders which don't connect to other cell borders at one end:
     # this is use full to get a clean structure of cell borders, which is later used to identifying the number and area of cells
     # applying remove endpoints multiple times to deal with forking dead ends
 
@@ -538,7 +541,7 @@ def find_borders(mask, shape, raise_error=True, type="colony", min_length=0):
         if type == "cell layer":
             c_l = Cells_and_Lines2(mask_boundaries, shape, graph, points)
             c_l.filter_small_de_line(min_length)
-    except (RecursionError, FindingBorderError, IndexError) as e:
+    except (RecursionError, FindingBorderError, IndexError, KeyError) as e:
         print("original error: ", e)
         if raise_error:
             raise FindingBorderError
@@ -1136,3 +1139,6 @@ if __name__ == "__main__":
     # make matrix with ids
     nodes, elements, loads, mats = grid_setup(mask_area, t_x, t_y, 1, 0.3)
     # plot_grid(nodes,elements,inverted_axis=True)  # only use with <1000 nodes
+
+#TODO: implement seperate mask to average stresses and stuff optionally even multiple stresses
+#TODO: Move away from recursive functions to identfy lines